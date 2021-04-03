d3002340313a5ee6a5b5b8938883d94baffeed2a
lifanova <veralifanovaa@gmail.com>
2021-04-03 23:38:12 +0300

    Post manager is added

---
+++ src/main/java/ru/netology/vk/manager/PostManager.java
@@ -0,0 +1,17 @@
+package ru.netology.vk.manager;
+
+import ru.netology.vk.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset) {
+
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+
+    }
+}
